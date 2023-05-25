8eb1a57ebec5aedda3c1088dff2a758668a5b59a
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-05-25 07:02:22 +0300

    correct way optimize search focus input

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -4,13 +4,13 @@ import { SearchContext } from '../../App';
 
 const Search = () => {
   const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const inputRef = React.useRef();
 
   const onClickClear = () => {
     setSearchValue('');
-    document.querySelector('input').focus();
+    inputRef.current.focus();
   };
 
- 
 
   return (
     <div className={styles.root}>
@@ -24,6 +24,7 @@ const Search = () => {
         <path d="M464,428,339.92,303.9a160.48,160.48,0,0,0,30.72-94.58C370.64,120.37,298.27,48,209.32,48S48,120.37,48,209.32s72.37,161.32,161.32,161.32a160.48,160.48,0,0,0,94.58-30.72L428,464ZM209.32,319.69A110.38,110.38,0,1,1,319.69,209.32,110.5,110.5,0,0,1,209.32,319.69Z" />
       </svg>
       <input
+        ref={inputRef}
         value={searchValue}
         onChange={(event) => setSearchValue(event.target.value)}
         className={styles.input}
