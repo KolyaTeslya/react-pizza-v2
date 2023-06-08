5d53c998c021ed6ecbe93c01ea76b64b1db824d7
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-06-08 06:56:46 +0300

    add  page for one pizza useParams uri

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 
 function FullPizza() {
+  const [pizza, setPizza] = React.useState(); 
   const { id } = useParams();
+
+
+
+  React.useEffect(() => {
+    async function fetchPizza(){
+      try {
+        const { data } = await axios.get('https://644fe2f3ba9f39c6ab6f03fb.mockapi.io/items/' + id);
+        setPizza(data);
+      } catch (error) {
+        alert('Ошибка при получении пиццы!');
+      }
+    }
+    fetchPizza();
+  },[])  
+
+  if(!pizza){
+    return 'Загрузка...';
+  }
 
   return (
   <div className='container'>
-    <img src='' />
-    <h2>{id}</h2>
-    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-    <h4>250 грн</h4>
+    <img src={pizza.imageUrl} />
+    <h2>{pizza.title}</h2>
+    <h4>{ pizza.price } грн</h4>
   </div>
   )
 }
