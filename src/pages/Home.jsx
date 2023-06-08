9b6769a11e463ae6b00511c250835346fe42f0b5
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-06-08 07:01:58 +0300

    fix get one pizza

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import qs from 'qs';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { setCategoryId, setCurrentPage, setFilters, selectFilter } from '../redux/slices/filterSlice';
 import Categories from '../components/Categories';
@@ -88,7 +88,7 @@ const Home = () => {
     }
   }, []);
 
-  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  const pizzas = items.map((obj) => <Link key={obj.id} to={`/pizza/${obj.id}`}><PizzaBlock {...obj} /></Link>);
 
   const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
